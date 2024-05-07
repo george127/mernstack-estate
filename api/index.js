ada10f19e393815e50890b77010fd04c18d0f66f
george127 <georgedarko127@yahoo.com>
2024-05-07 15:17:37 +0000

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config(); 
 
@@ -26,6 +27,7 @@ app.get('/test', (req, res) => {
 
 // Routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
