8cd1baf8d063cdb210075ab047d544f1682af2e8
george127 <georgedarko127@yahoo.com>
2024-05-08 13:19:03 +0000

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import './SignUp.css'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className='container'>
+    <div className='signup-container'>
+    <h1 className='signup-title'>Sign Up</h1>
+      
+      <form  className='signup-form'>
+        <input
+          type='text'
+          placeholder='Username'
+          className='signup-input'
+          id='username'
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          className='signup-input'
+          id='email'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='signup-input'
+          id='password'
+        />
+        <button className="btn bg-rose-200">Sign up</button>
+      </form>
+      <div className='signup-login'>
+        <p>Have an account?</p>
+         <Link  to={'/sign-in'}>
+         <span>Sign in</span>
+         </Link>
+      </div>
+    </div>
+    </div>
+  );
+  
 }
