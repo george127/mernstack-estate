eaac429418c1bddad478f9bd0e96fc43c62baaaf
george127 <georgedarko127@yahoo.com>
2024-06-01 14:10:41 +0000

    complete header search form functionality

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,52 +1,72 @@
-import { Link } from 'react-router-dom';
-import './Header.css';
-import { useSelector } from 'react-redux';
-
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.css";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
-    <header className='header'>
-        <div className='container'>
-        <Link to='/'>
-        <h1 className='logo'>
-            <span className='text-rose-200'>Mern</span>
-            <span className=''>Estate</span>
-        </h1> 
+    <header className="header">
+      <div className="container">
+        <Link to="/">
+          <h1 className="logo">
+            <span className="text-rose-200">Mern</span>
+            <span className="">Estate</span>
+          </h1>
         </Link>
-        <form className='forms-container'>
-            <input type='text' placeholder='Type to search...' className='input bg-rose-200' />
-            <span className="material-symbols-outlined">search</span>        
+        <form className="forms-container" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type to search..."
+            className="input bg-rose-200" value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+          <span className="material-symbols-outlined">search</span>
+          </button>
         </form>
-        <ul className='link-container'>
-          <Link to='/'>
-          <li className='nav-item text-red-200'>
-            <p className='nav-link'>Home</p>
-          </li>
-          </Link>
-          
-          <Link to='/about'>
-          <li className='nav-item text-red-200'>
-            <p className='nav-link'>About</p>
-          </li>
+        <ul className="link-container">
+          <Link to="/"> 
+            <li className="nav-item text-red-200">
+              <p className="nav-link">Home</p>
+            </li>
           </Link>
 
-        <Link to='/profile'>
+          <Link to="/about">
+            <li className="nav-item text-red-200">
+              <p className="nav-link">About</p>
+            </li>
+          </Link>
+
+          <Link to="/profile">
             {currentUser ? (
-              <img
-                className='Image'
-                src={currentUser.avatar}
-                alt='profile'
-              />
+              <img className="Image" src={currentUser.avatar} alt="profile" />
             ) : (
-              <li className='nav-item text-red-200'>
-            <p className='nav-link'>Sign In</p>
-          </li>
+              <li className="nav-item text-red-200">
+                <p className="nav-link">Sign In</p>
+              </li>
             )}
           </Link>
         </ul>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
