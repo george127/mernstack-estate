8ddc3a9daea407d1797c9b7c7558ecb4f6f11004
george127 <georgedarko127@yahoo.com>
2024-06-03 09:09:45 +0000

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,8 +5,11 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
-
+import path from 'path';
 dotenv.config(); 
+
+
+const __dirname = path.resolve();
 
 const app = express(); 
 
@@ -33,6 +36,12 @@ app.get('/test', (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 // Start the server
 const PORT = process.env.PORT || 3000;
