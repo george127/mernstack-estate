a3bde29d2933be25e9b8f9ae21048c8f9a998476
George <georgedarko127@gmail.com>
2025-09-05 13:13:59 +0000

    Empty commit with no code changes made.

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-82cfc.firebaseapp.com",
-  projectId: "mern-estate-82cfc",
-  storageBucket: "mern-estate-82cfc.appspot.com",
-  messagingSenderId: "803062241998",
-  appId: "1:803062241998:web:0153e8bf5d50efc9604e9f"
+  authDomain: "mernproject-919e4.firebaseapp.com",
+  databaseURL: "https://mernproject-919e4-default-rtdb.firebaseio.com",
+  projectId: "mernproject-919e4",
+  storageBucket: "mernproject-919e4.firebasestorage.app",
+  messagingSenderId: "981952093507",
+  appId: "1:981952093507:web:19c5acb2bd00249d0263f6",
+  measurementId: "G-SDCX0JMZX3"
 };
 
 // Initialize Firebase
